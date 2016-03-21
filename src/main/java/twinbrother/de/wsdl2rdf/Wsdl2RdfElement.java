20f5329e5910fdcd728b8f982fc8176653a1f13e
Philipp Kraus <philipp@ebkraus.de>
2016-03-21 17:02:14 +0100

    Added method to retrieve targetNamespace of imported WSDL document

--- src/main/java/twinbrother/de/wsdl2rdf/Wsdl2RdfElement.java
+++ src/main/java/twinbrother/de/wsdl2rdf/Wsdl2RdfElement.java
@@ -1,12 +1,29 @@
 package twinbrother.de.wsdl2rdf;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+
+import org.openrdf.model.Value;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.sail.memory.MemoryStore;
 
 public class Wsdl2RdfElement {
 
@@ -24,7 +41,8 @@ public class Wsdl2RdfElement {
 
 	/**
 	 * Process the given wsdlFile to rdf
-	 * @throws TransformerException 
+	 * 
+	 * @throws TransformerException
 	 */
 	private void process(File wsdlFile) throws TransformerException {
 
@@ -34,9 +52,55 @@ public class Wsdl2RdfElement {
 	}
 
 	/**
+	 * Retrieves the targetNameSpace of the imported WSDL Document
+	 * 
+	 * @return String the targetNamespace of the given WSDL Document
+	 * @throws RepositoryException
+	 * @throws IOException
+	 * @throws RDFParseException
+	 * @throws QueryEvaluationException 
+	 * @throws MalformedQueryException 
+	 */
+	public String getTargetNamespace() throws RepositoryException, RDFParseException, IOException, QueryEvaluationException, MalformedQueryException {
+
+		String targetNamespace = "";
+		
+		Repository repo = new SailRepository(new MemoryStore());
+		repo.initialize();
+
+		RepositoryConnection con = repo.getConnection();
+
+		try {
+			con.add(geRdfXmlFile(), "http://TODO", RDFFormat.RDFXML);
+			String queryString = "PREFIX rwsdl: <http://www.w3.org/2005/10/wsdl-rdf#> "
+					+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+					+ "SELECT DISTINCT * where {?s rdf:type rwsdl:Description}";
+			TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+
+			TupleQueryResult result = tupleQuery.evaluate();
+			try {
+				while (result.hasNext()) { // iterate over the result
+					BindingSet bindingSet = result.next();
+					String valueOfS = bindingSet.getValue("s").stringValue();
+					
+					targetNamespace = valueOfS.split("#")[0];
+					
+					break;
+				}
+			} finally {
+				result.close();
+			}
+		} finally {
+			con.close();
+		}
+
+		return targetNamespace;
+	}
+
+	/**
 	 * Returns the created RDF/XML file
 	 * 
-	 * @return
+	 * @return an RDF/XML File
 	 */
 	public File geRdfXmlFile() {
 
--- src/test/java/twinbrother/de/wsdl2rdf/Wsdl2RdfElementTest.java
+++ src/test/java/twinbrother/de/wsdl2rdf/Wsdl2RdfElementTest.java
@@ -10,36 +10,60 @@ public class Wsdl2RdfElementTest {
 
 	@Test
 	public void testWsdl2RdfElement() {
-		
-		
-		
+
 	}
 
 	@Test
 	public void testGeRdfXmlFile() {
-		
+
 		ClassLoader classLoader = getClass().getClassLoader();
 		File wsdlLocation = new File(classLoader.getResource("test.wsdl").getFile());
-		
+		File testFile = null;
+
 		Wsdl2RdfInterface wsdl2rdf = new Wsdl2Rdf(new File("/GovRepDataFolder/"));
 		try {
 			Wsdl2RdfElement element = wsdl2rdf.importsingleFile(wsdlLocation);
-			element.geRdfXmlFile();
+			testFile = element.geRdfXmlFile();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		File outputfile = new File(Wsdl2Rdf.getWorkingDirectory().getAbsolutePath() + "/test.rdf");
-		System.out.println(outputfile.getAbsolutePath());
-		
-		assertTrue(outputfile.exists());
-		
-		//outputfile.delete();
+
+		assertTrue(testFile.getAbsolutePath().equals(outputfile.getAbsolutePath()));
+
+		if (testFile != null) {
+			testFile.delete();
+		}
 	}
 
 	@Test
 	public void testGetRdfTurtle() {
+
+	}
+
+	@Test
+	public void testGetTargetNamespace() {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File wsdlLocation = new File(classLoader.getResource("test.wsdl").getFile());
+		String tns = "";
+		File testFile = null;
+
+		Wsdl2RdfInterface wsdl2rdf = new Wsdl2Rdf(new File("/GovRepDataFolder/"));
+		try {
+			Wsdl2RdfElement element = wsdl2rdf.importsingleFile(wsdlLocation);
+			testFile = element.geRdfXmlFile();
+			tns = element.getTargetNamespace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(tns.equals("http://www.herongyang.com/Service/v1/"));
+		
+		if (testFile != null) {
+			testFile.delete();
+		}
 		
 	}
 
